1266bb05802404016120abb1e644588d6ef2bad6
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-05-23 16:27:22 +0530

    [Bhgya] problem 2 harmonic number

--- 2problem.sh
+++ 2problem.sh
@@ -1,21 +1,11 @@
 #! /bin/bash -x
 
-d=$((RANDOM % 7 + 1))
-case $d in
-	1)
-	echo "monday";;
-	2)
-	echo "tuesday";;
-	3)
-	echo "wednesday";;
-	4)
-	echo "thursday";;
-	5)
-	echo "friday";;
-	6)
-	echo "saturday";;
-	7)
-	echo "sunday";;
-	*)
-	echo "invalid";;
-esac
+
+read -p "enter the number" n
+for((i=1;i<=n;i++))
+do
+	sum=$sum+1/$i 
+done
+
+echo $sum
+
