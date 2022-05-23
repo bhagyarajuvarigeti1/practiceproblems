765f6a35ec3dd57793ad2f5d72a39387255bba28
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-05-23 05:49:18 +0530

    [Bhagya} storing in dictionary

--- sortintarithmeticcomputatainproblem.sh
+++ sortintarithmeticcomputatainproblem.sh
@@ -1,12 +1,15 @@
 #! /bin/bash -x
 
+declare -A dixt
+
+
 read -p "first number" a
 read -p "second number" b
 read -p "third number" c
 
-sol=$(($a+$b*$c))
-sol1=$(($a*$b+$c))
-sol2=$(($a+$b/$c))
-echo $a " + " $b " * " $c " = " $sol
-echo $a " * " $b " + " $c " = "$sol1
-echo $a " + " $b " / " $c " = "$sol2
+dixt[0]=$(($a+$b*$c))
+dixt[1]=$(($a*$b+$c))
+dixt[2]=$(($a+$b/$c))
+dixt[3]=$(($a%$b+$c))
+
+echo ${dixt[@]}
