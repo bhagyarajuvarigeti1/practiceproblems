6ad144efb84e73c22b77dc8a3ea2cd1f5d10d01b
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-05-23 16:26:31 +0530

    [Bhagya] powers of 2

--- problem1.sh
+++ problem1.sh
@@ -1,28 +1,9 @@
 #! /bin/bash -x
 
-a=$((RANDOM % 9))
-case $a in
-	0)
-		echo "zero";;
-	1)
-		echo "one";;
-	2)
-		echo "two";;
-	3)
-		echo "three";;
-	4)
-		echo "four";;
-	5)
-		echo "five";;
-	6)
-		echo "six";;
-	7)
-		echo "seven";;
-	8)
-		echo "eight";;
-	9)
-		echo "nine";;
-	*)
-		echo "invalid"
-		;;
-esac
+read -p "enter number" n
+ref=1
+for((i=0;i<n;i++))
+do
+	ref=$(($ref*2))
+done
+
